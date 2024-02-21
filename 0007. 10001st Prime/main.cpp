ff249ce2dd07107779f19fbab9581a3856536e87
Alipervaiz <syedaliakrampervaiz@gmail.com>
2024-02-20 21:37:24 -0500

    02/20/2024

---
+++ 0007. 10001st Prime/main.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+using namespace std;
+ 
+#define rep(i,a,b) for(auto i=(a); i<(b); ++i)
+#define repc(i,a,b,c) for(auto i=(a); i<(b); i+=c)
+#define repn(i,a,b) for(auto i=(a); i>(b); --i)
+#define trav(a,x) for(auto& a: x)
+#define travm(a,x) for(const auto& [a, v]: x)
+#define all(x) begin(x),end(x)
+#define sz(x) (int)size(x)
+#define madd(a,b) (a+b)%MOD
+#define mm(a,b) ((ll)a*b)%MOD
+#define tmod(a,b) ((a%b)+b)%b
+#define pb push_back
+#define mp make_pair
+#define cauto const auto
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+ll MOD = 1e9+7;
+
+ll modPow(ll x, ll y) 
+{ 
+    ll res = 1; x %= MOD;
+    if(x==0) return 0;
+    while(y>0) 
+    { 
+        if(y&1) res=mm(res,x); 
+        y = y>>1; x=mm(x,x); 
+    } 
+    return res; 
+} 
+int aOvf(ll a, ll b)
+{ 
+  ll temp; 
+  return __builtin_add_overflow(a,b,&temp);
+}
+int mOvf(ll a, ll b)
+{
+  ll temp; 
+  return __builtin_mul_overflow(a,b,&temp);
+}
+
+auto stopwatch_tick() {
+  return chrono::high_resolution_clock::now();
+}
+ 
+auto stopwatch_duration(auto st, auto en) {
+  return chrono::duration_cast<chrono::milliseconds>(en - st).count();
+}
+struct node
+{
+  ll l;
+  ll r;
+  // bool operator<(const node& o) const
+  // {
+  //   return l>o.l;
+  // }
+};
+ll INF = numeric_limits<ll>::max();
+
+int f(int n)
+{
+  rep(i,2,sqrt(n)+1) if(n%i==0&&n!=i) return 0;
+  return 1;
+}
+void solve()
+{
+  int p = 0;
+  int i = 2;
+  while(p!=10001) if(f(i++)) p++;
+  cout << i-1;
+}
+void pre()
+{
+}
+int32_t main() 
+{
+  pre();
+  solve();
+}
---
+++ 0008. Largest Product in a Series/main.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+using namespace std;
+ 
+#define rep(i,a,b) for(auto i=(a); i<(b); ++i)
+#define repc(i,a,b,c) for(auto i=(a); i<(b); i+=c)
+#define repn(i,a,b) for(auto i=(a); i>(b); --i)
+#define trav(a,x) for(auto& a: x)
+#define travm(a,x) for(const auto& [a, v]: x)
+#define all(x) begin(x),end(x)
+#define sz(x) (int)size(x)
+#define madd(a,b) (a+b)%MOD
+#define mm(a,b) ((ll)a*b)%MOD
+#define tmod(a,b) ((a%b)+b)%b
+#define pb push_back
+#define mp make_pair
+#define cauto const auto
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+ll MOD = 1e9+7;
+
+ll modPow(ll x, ll y) 
+{ 
+    ll res = 1; x %= MOD;
+    if(x==0) return 0;
+    while(y>0) 
+    { 
+        if(y&1) res=mm(res,x); 
+        y = y>>1; x=mm(x,x); 
+    } 
+    return res; 
+} 
+int aOvf(ll a, ll b)
+{ 
+  ll temp; 
+  return __builtin_add_overflow(a,b,&temp);
+}
+int mOvf(ll a, ll b)
+{
+  ll temp; 
+  return __builtin_mul_overflow(a,b,&temp);
+}
+
+auto stopwatch_tick() {
+  return chrono::high_resolution_clock::now();
+}
+ 
+auto stopwatch_duration(auto st, auto en) {
+  return chrono::duration_cast<chrono::milliseconds>(en - st).count();
+}
+struct node
+{
+  ll l;
+  ll r;
+  // bool operator<(const node& o) const
+  // {
+  //   return l>o.l;
+  // }
+};
+ll INF = numeric_limits<ll>::max();
+
+int f(int n)
+{
+  rep(i,2,sqrt(n)+1) if(n%i==0&&n!=i) return 0;
+  return 1;
+}
+void solve()
+{
+  string s = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+  int n = 13;
+  ll best = 0;
+  rep(i,0,sz(s))
+  {
+    if(i<n-1) continue;
+    ll prod = 1;
+    rep(j,i-n+1,i+1) prod *= s[j]-'0';
+    best = max(best,prod);
+  }
+  cout << best;
+}
+void pre()
+{
+}
+int32_t main() 
+{
+  pre();
+  solve();
+}
